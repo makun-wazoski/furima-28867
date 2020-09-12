7069c385d752b630985a7e33544b1570ac4699a6
makun-wazoski <mo19850226@gmail.com>
2020-09-12 16:14:10 +0900

    商品購入リンク機能　完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    # @user = User.find(params[:nickname])
   end
 
   def  done
