c6b885b5c66c74c1e5501f3e65b63b0eebf839b2
makun-wazoski <mo19850226@gmail.com>
2020-09-14 17:37:10 +0900

    出品者自身の商品購入ページ遷移不可

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,17 @@
 class PurchasesController < ApplicationController
+  before_action :move_to_index, only: [:index]
 
   def index
     @item = Item.find(params[:item_id])
   end
   
   def create
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
