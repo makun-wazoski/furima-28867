b733a89b3b30df885f1cf4caad9b41da21a949c2
makun-wazoski <mo19850226@gmail.com>
2020-09-14 20:21:47 +0900

    購入済みページへ遷移不可

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :move_to_index, only: [:index]
-
+  before_action :move_to_index2, only: [:index]
+  
   def index
     @item = Item.find(params[:item_id])
   end
@@ -11,6 +12,16 @@ class PurchasesController < ApplicationController
   def move_to_index
     @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def move_to_index2
+    # params[:item_id]はurlのitems/item_id/purchaseを取得している
+    # ここではItemテーブルに合致する情報を取得している
+    @purchase = Item.find(params[:item_id])
+    if @purchase.purchase != nil
+      # アソシエーション↑で結びつける
       redirect_to root_path
     end
   end
