ebba73b9d789fc90a979d063e3829be0235cfd0b
makun-wazoski <mo19850226@gmail.com>
2020-09-24 12:00:56 +0900

    商品検索機能　改善版2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy, :seach]
 
   def index
-    @items = Item.all.order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
